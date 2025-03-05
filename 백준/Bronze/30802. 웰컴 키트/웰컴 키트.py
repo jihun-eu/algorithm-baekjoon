1009cccfeaae477a996dd83ad28ccbe5115b410f
jihun.eu <hilton3180@gmail.com>
2025-03-05 14:05:50 +0900

    [Bronze III] Title: 웰컴 키트, Time: 184 ms, Memory: 119164 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import sys
 import math
+from typing import List, Tuple
+
+input = sys.stdin.readline
+
+
+def minBought(participants: int, sizeOfParticipants: List[int], tshirts: int, pens: int) -> Tuple[int, int, int]:
+
+    orderTshirts = sum(math.ceil(sizeOfParticipant / tshirts) for sizeOfParticipant in sizeOfParticipants)
+    orderBunchOfPens = participants // pens
+    orderPens = participants % pens
+
+    return orderTshirts, orderBunchOfPens, orderPens
+
 
 def main():
-    participants = int(sys.stdin.readline().rstrip())
-    sizes = list(map(int, sys.stdin.readline().rstrip().split()))
-    pack_shirt, pack_pen = map(int, sys.stdin.readline().rstrip().split())
+    participants = int(input())
+    sizeOfParticipants = list(map(int, input().split()))
+    tshirts, pens = map(int, input().split())
 
-    pack_of_shirts_to_buy = sum([math.ceil(s / pack_shirt) for s in sizes])
-    pack_of_pens_to_buy = participants // pack_pen
-    single_pens_to_buy = participants % pack_pen
+    orderTshirts, orderBunchOfPens, orderPens = minBought(participants, sizeOfParticipants, tshirts, pens)
 
-    print(pack_of_shirts_to_buy)
-    print(pack_of_pens_to_buy, single_pens_to_buy)
-    
-
+    print(orderTshirts)
+    print(orderBunchOfPens, orderPens)
 
 if __name__ == "__main__":
     main()
